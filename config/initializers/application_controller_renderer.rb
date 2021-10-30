f5971f7542c96f452cca79ee5188b2239dd52262
Kabaddi4 <ec2-user@ip-172-31-60-239.ec2.internal>
2021-10-30 03:15:54 +0000

    end user show edit

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '90480ba2b1c7a0dd8103059e5c31e9e164deaa58ce5e8ed068cb60f6a931d8e5b576005084440045c2aacdfcc4fb0c5817c556f0af95de64b9a67e7cf2b41933'
